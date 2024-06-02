dc994ea0d59fc2c48b407c929754ea2a37a72bc4
roseghanem <moza1541998@gmail.com>
2024-06-02 23:23:39 +0300

    Deployment

--- public/ds/SignUp.jsx
+++
@@ -1,9 +0,0 @@
-import htmlContent from '../htmls/signup.html';
-
-const HtmlSignUpContent = () => {
-  return (
-    <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
-  );
-};
-
-export default HtmlSignUpContent;
--- public/ds/VerificationCode.jsx
+++
@@ -1,9 +0,0 @@
-import htmlContent from '../htmls/verification.html';
-
-const HtmlVerificationContent = () => {
-  return (
-    <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
-  );
-};
-
-export default HtmlVerificationContent;
